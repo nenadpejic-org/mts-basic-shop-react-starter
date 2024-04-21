53a395f4c1d343af1d618ceb29915e061d32a72f
Nenad Pejic <nenad.p.pejic@gmail.com>
2024-04-21 17:52:25 +0200

    feat(lesson-4): json server and concurrently

---
+++ src/services/jsonApi.ts
@@ -0,0 +1,51 @@
+const BASE_URL = 'http://localhost:3000'
+
+type Query = Record<string, string | boolean | number | object>
+
+const getQueryParams = (query?: Query) => {
+  if (!query) return ''
+  const filteredQuery = Object.fromEntries(
+    Object.entries(query).filter(([, v]) => v !== ''),
+  )
+  if (Object.keys(filteredQuery).length === 0) return ''
+
+  return `?${Object.entries(filteredQuery)
+    .map(([key, value]) => `${key}=${value}`, [])
+    .join('&')}`
+}
+
+type JsonApiProps = {
+  endpoint: string
+  init?: RequestInit
+  query?: Query
+  baseUrl?: string
+}
+
+export const jsonApi = async <JsonApiRes>({
+  endpoint,
+  init,
+  query,
+  baseUrl = BASE_URL,
+}: JsonApiProps): Promise<JsonApiRes> => {
+  try {
+    const url = `${baseUrl}${endpoint}${getQueryParams(query)}`
+    const res = await fetch(url, init)
+
+    if (!res.ok) {
+      let resError
+      resError = (await res.json()) as Record<string, never>
+      if (!Object.keys(resError).length) {
+        resError = {
+          status: res.status,
+          message: res.statusText || 'Unhandled json-server error',
+        }
+      }
+      return Promise.reject(resError)
+    }
+
+    const resData = (await res.json()) as JsonApiRes
+    return resData
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
