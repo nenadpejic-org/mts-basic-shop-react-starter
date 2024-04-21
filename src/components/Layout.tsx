deffdd3eba84002a2a44af588f454ebb0624081e
Nenad Pejic <nenad.p.pejic@gmail.com>
2024-04-21 14:10:14 +0200

    feat(lesson-3): routing

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+import Header, { HeaderType } from './Header'
+
+type Props = {
+  headerType?: HeaderType
+}
+
+const Layout = ({ headerType }: Props) => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header type={headerType} />
+
+      <main className="mt-[72px] grow">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,29 @@
+const HomePage = () => {
+  return (
+    <section className="relative h-[calc(100svh-72px)]">
+      <img
+        className="aspect-[16/9] h-full w-full object-cover object-center"
+        src="https://source.unsplash.com/man-in-black-jacket-and-brown-pants-lying-on-snow-covered-ground-during-daytime-q80sx583gzE"
+        alt=""
+      />
+      <div className="container absolute inset-0 flex flex-col justify-end py-6">
+        <h1 className="text-2xl font-bold text-gray-900">
+          Experience the Thrill
+        </h1>
+        <p className="font-medium text-gray-900">
+          Elevate Your Snowboarding Adventure with Us!
+        </p>
+        <div>
+          <a
+            className="mt-5 inline-block bg-white px-6 py-2 font-medium text-gray-900"
+            href="/shop"
+          >
+            Shop
+          </a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HomePage
