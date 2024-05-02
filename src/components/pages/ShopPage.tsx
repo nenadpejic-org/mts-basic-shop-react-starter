8a53d34a6bd8780dc9f386ae084747978f63e88b
Nenad Pejic <nenad.p.pejic@gmail.com>
2024-05-02 21:21:39 +0200

    feat(lesson-6): fetching data useReducer

--- src/components/pages/ShopPage.tsx
+++ src/components/pages/ShopPage.tsx
@@ -1,5 +1,5 @@
 import { Product, getProducts } from '@/services/products'
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer } from 'react'
 
 type GetProductsQuery = {
   isLoading: boolean
@@ -13,35 +13,59 @@ const defaultGetProductsQuery: GetProductsQuery = {
   data: null,
 }
 
+type Action =
+  | { type: 'getProductsQuery.init' }
+  | { type: 'getProductsQuery.error'; payload: string }
+  | { type: 'getProductsQuery.success'; payload: Product[] }
+
+const queryReducer = (
+  state: GetProductsQuery,
+  action: Action,
+): GetProductsQuery => {
+  switch (action.type) {
+    case 'getProductsQuery.init':
+      return {
+        isLoading: true,
+        error: '',
+        data: null,
+      }
+    case 'getProductsQuery.error':
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      }
+    case 'getProductsQuery.success':
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload,
+      }
+  }
+}
+
 const ShopPage = () => {
-  const [getProductsQuery, setGetProductsQuery] = useState<GetProductsQuery>(
-    defaultGetProductsQuery,
-  )
+  const [state, dispatch] = useReducer(queryReducer, defaultGetProductsQuery)
 
   useEffect(() => {
     const fetch = async () => {
       try {
-        setGetProductsQuery({ isLoading: true, error: '', data: null })
-        const products = await getProducts({ query: { availability: true } })
-        setGetProductsQuery((pV) => ({
-          ...pV,
-          isLoading: false,
-          data: products,
-        }))
+        dispatch({ type: 'getProductsQuery.init' })
+        const data = await getProducts({ query: { availability: true } })
+        dispatch({ type: 'getProductsQuery.success', payload: data })
       } catch (error) {
         const { message } = error as Error
-        setGetProductsQuery((pV) => ({
-          ...pV,
-          isLoading: false,
-          error: message,
-        }))
+        dispatch({
+          type: 'getProductsQuery.error',
+          payload: message,
+        })
       }
     }
 
     fetch()
   }, [])
 
-  console.log(getProductsQuery)
+  console.log(state)
 
   return <></>
 }
