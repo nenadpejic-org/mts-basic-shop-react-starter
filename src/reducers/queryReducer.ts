3bf713ba3376354ae8970ea83e357152c1568138
Nenad Pejic <nenad.p.pejic@gmail.com>
2024-05-02 22:40:36 +0200

    feat(lesson-7): fetching data useContext

--- src/reducers/queryReducer.ts
+++ src/reducers/queryReducer.ts
@@ -1,58 +1,45 @@
 import { Product } from '@/services/products'
 
 type QueryReducerState = {
-  getProductsQuery: {
-    isLoading: boolean
-    error: string
-    data: null | Product[]
-  }
+  isLoading: boolean
+  error: string
+  data: null | Product[]
 }
 
 const defaultQueryReducerState: QueryReducerState = {
-  getProductsQuery: {
-    isLoading: false,
-    error: '',
-    data: null,
-  },
+  isLoading: false,
+  error: '',
+  data: null,
 }
 
 type Action =
-  | { type: 'getProductsQuery.init' }
-  | { type: 'getProductsQuery.error'; payload: string }
-  | { type: 'getProductsQuery.success'; payload: Product[] }
+  | { type: 'init' }
+  | { type: 'error'; payload: string }
+  | { type: 'success'; payload: Product[] }
 
 const queryReducer = (
   state: QueryReducerState,
   action: Action,
 ): QueryReducerState => {
   switch (action.type) {
-    case 'getProductsQuery.init':
+    case 'init':
       return {
         ...state,
-        getProductsQuery: {
-          ...state.getProductsQuery,
-          isLoading: true,
-          error: '',
-          data: null,
-        },
+        isLoading: true,
+        error: '',
+        data: null,
       }
-    case 'getProductsQuery.error':
+    case 'error':
       return {
         ...state,
-        getProductsQuery: {
-          ...state.getProductsQuery,
-          isLoading: false,
-          error: action.payload,
-        },
+        isLoading: false,
+        error: action.payload,
       }
-    case 'getProductsQuery.success':
+    case 'success':
       return {
         ...state,
-        getProductsQuery: {
-          ...state.getProductsQuery,
-          isLoading: false,
-          data: action.payload,
-        },
+        isLoading: false,
+        data: action.payload,
       }
   }
 }
