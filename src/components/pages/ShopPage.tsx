0be2e5b2edb1c28755d402e38ca18426a1db3196
Nenad Pejic <nenad.p.pejic@gmail.com>
2024-05-02 21:08:53 +0200

    feat(lesson-5): fetching data useState

--- src/components/pages/ShopPage.tsx
+++ src/components/pages/ShopPage.tsx
@@ -1,4 +1,48 @@
+import { Product, getProducts } from '@/services/products'
+import { useEffect, useState } from 'react'
+
+type GetProductsQuery = {
+  isLoading: boolean
+  error: string
+  data: null | Product[]
+}
+
+const defaultGetProductsQuery: GetProductsQuery = {
+  isLoading: false,
+  error: '',
+  data: null,
+}
+
 const ShopPage = () => {
+  const [getProductsQuery, setGetProductsQuery] = useState<GetProductsQuery>(
+    defaultGetProductsQuery,
+  )
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        setGetProductsQuery({ isLoading: true, error: '', data: null })
+        const products = await getProducts({ query: { availability: true } })
+        setGetProductsQuery((pV) => ({
+          ...pV,
+          isLoading: false,
+          data: products,
+        }))
+      } catch (error) {
+        const { message } = error as Error
+        setGetProductsQuery((pV) => ({
+          ...pV,
+          isLoading: false,
+          error: message,
+        }))
+      }
+    }
+
+    fetch()
+  }, [])
+
+  console.log(getProductsQuery)
+
   return <></>
 }
 
