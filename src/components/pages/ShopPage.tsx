dd946662892ae6497bc67f09180f42916cc5a7e1
Nenad Pejic <nenad.p.pejic@gmail.com>
2024-05-14 00:24:10 +0200

    feat(lesson-10): shop page - heading, input, products list

--- src/components/pages/ShopPage.tsx
+++ src/components/pages/ShopPage.tsx
@@ -1,10 +1,13 @@
 import { useQuery } from '@/hooks/useQuery'
 import { Product } from '@/services/products'
 import { useEffect, useState } from 'react'
+import { ProductsList } from '../ProductsList'
+import Heading from '../ui/Heading'
+import Icon from '../ui/Icon'
 
 const ShopPage = () => {
   const query = useQuery()
-  const [products, setProducts] = useState<Product[]>([])
+  const [products, setProducts] = useState<null | Product[]>(null)
 
   useEffect(() => {
     query.fetch({
@@ -16,9 +19,32 @@ const ShopPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  console.log(products)
+  return (
+    <section className="py-8">
+      <div className="container">
+        <Heading>Explore our Shop: Elevate Your Ride</Heading>
 
-  return <></>
+        <div className="relative mt-8">
+          <Icon
+            icon="search"
+            className="pointer-events-none absolute left-2 top-1/2 -translate-y-1/2"
+          />
+          <input
+            className="w-full border border-gray-400 bg-gray-50 px-4 py-2 pl-10 text-gray-900 placeholder:text-gray-400 disabled:cursor-not-allowed disabled:opacity-50 md:w-auto"
+            type="text"
+            placeholder="Search products"
+          />
+        </div>
+
+        <ProductsList
+          className="mt-8"
+          products={products}
+          error={query.error}
+          isLoading={query.isLoading}
+        />
+      </div>
+    </section>
+  )
 }
 
 export default ShopPage
---
+++ src/components/ui/Icon.tsx
@@ -0,0 +1,22 @@
+import search from '@/assets/search.svg?react'
+import shopingCart from '@/assets/shopping_cart.svg?react'
+import { HTMLAttributes } from 'react'
+
+const iconMap = {
+  search,
+  shopingCart,
+}
+
+type IconName = keyof typeof iconMap
+
+type Props = HTMLAttributes<HTMLOrSVGElement> & {
+  icon: IconName
+}
+
+const Icon = ({ icon, ...rest }: Props) => {
+  const Icon = iconMap[icon]
+
+  return <Icon {...rest} />
+}
+
+export default Icon
