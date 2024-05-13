f5b1caa53bb7830d6db3c19ed9bc00a743c479f3
Nenad Pejic <nenad.p.pejic@gmail.com>
2024-05-14 00:24:10 +0200

    feat(lesson-9): shop page - heading, input, products list

--- src/components/pages/ShopPage.tsx
+++ src/components/pages/ShopPage.tsx
@@ -1,17 +1,30 @@
 import { useQuery } from '@/hooks/useQuery'
 import { useEffect } from 'react'
+import { ProductsList } from '../ProductsList'
+import Heading from '../ui/Heading'
 
 const ShopPage = () => {
   const query = useQuery()
 
   useEffect(() => {
-    query.fetch()
+    query.fetch({ options: { query: { availability: true } } })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  console.log(query)
+  return (
+    <section className="py-8">
+      <div className="container">
+        <Heading>Explore our Shop: Elevate Your Ride</Heading>
 
-  return <></>
+        <ProductsList
+          className="mt-8"
+          products={query.data}
+          error={query.error}
+          isLoading={query.isLoading}
+        />
+      </div>
+    </section>
+  )
 }
 
 export default ShopPage
