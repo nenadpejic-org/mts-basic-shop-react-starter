eeee4b07e155fbec58a375e83ab5911701cdc2ad
Nenad Pejic <nenad.p.pejic@gmail.com>
2024-05-14 01:09:01 +0200

    feat(lesson-9): filtering

--- src/components/pages/ShopPage.tsx
+++ src/components/pages/ShopPage.tsx
@@ -1,15 +1,22 @@
 import { useQuery } from '@/hooks/useQuery'
-import { useEffect } from 'react'
+import { Product } from '@/services/products'
+import { useEffect, useState } from 'react'
 
 const ShopPage = () => {
   const query = useQuery()
+  const [products, setProducts] = useState<Product[]>([])
 
   useEffect(() => {
-    query.fetch()
+    query.fetch({
+      onSuccess: (products) => {
+        const availableProducts = products.filter((p) => p.availability)
+        setProducts(availableProducts)
+      },
+    })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  console.log(query)
+  console.log(products)
 
   return <></>
 }
